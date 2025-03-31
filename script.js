75552f97e815fc91787df17a18c2c75f3a953249
Frederic Alleron <117657605+fr3d0r1c@users.noreply.github.com>
2025-03-31 12:59:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Fonction qui démarre l'animation de clignotement au chargement de la page
+window.onload = function() {
+    var alertBox = document.getElementById("alertBox");
+    // Ajouter la classe d'animation au panneau d'alerte
+    alertBox.classList.add("blink-animation");
+
+    // Pour arrêter l'animation après 10 secondes
+    setTimeout(function() {
+        alertBox.classList.remove("blink-animation");
+    }, 10000); // L'animation s'arrête après 10 secondes
+};
 
